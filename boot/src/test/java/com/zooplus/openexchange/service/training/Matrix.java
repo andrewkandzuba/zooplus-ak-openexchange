4d070da7e9e41c08e02f7b15763cfc90e0facd6a
andrew_kandzuba <andrew_kandzuba2@OSX.lan>
2016-03-23 19:49:20 +0200

    more training uses cases added

---
+++ boot/src/test/java/com/zooplus/openexchange/service/training/Matrix.java
@@ -0,0 +1,58 @@
+package com.zooplus.openexchange.service.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Matrix {
+    private static final Logger logger = LoggerFactory.getLogger(Matrix.class);
+
+
+    private int[][] m;
+
+    protected Matrix(int size) {
+        this.m = new int[size][size];
+        int v = 0;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                m[col][row] = v++;
+            }
+        }
+    }
+
+    public static Matrix init(int size){
+        return new Matrix(size);
+    }
+
+    public Matrix rotate90(){
+        int N = m.length;
+        for(int row = 0; row < (N/2); row++){
+            for(int col = 0; col < N; col++){
+                int temp = m[col][N - row - 1];
+                m[col][N - row - 1] = m[col][row];
+                m[col][row] = temp;
+            }
+        }
+        logger.info(toString());
+        for(int c = 0; c < N; c++){
+            for(int r = c;  r < N; r++){
+                int temp =  m[c][r];
+                m[c][r] = m[r][c];
+                m[r][c] = temp;
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int col = 0; col < m.length; col++){
+            sb.append(String.format("%n"));
+            for(int row = 0; row < m.length; row++){
+                sb.append(String.format("|%-4s",m[row][col]));
+            }
+            sb.append("|");
+        }
+        return sb.toString();
+    }
+}
---
+++ boot/src/test/java/com/zooplus/openexchange/service/training/TestMatrixOperations.java
@@ -0,0 +1,31 @@
+package com.zooplus.openexchange.service.training;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMatrixOperations {
+    private static final Logger logger = LoggerFactory.getLogger(TestMatrixOperations.class);
+
+    @Test
+    public void testArrayShift() throws Exception {
+        RingInt.from(new int[]{1, 2, 3}).shift(1).equalsTo(new int[]{3, 2, 1});
+        RingInt.from(new int[]{1, 2, 3}).shift(12).equalsTo(new int[]{1, 2, 3});
+        RingInt.from(new int[]{1, 2, 3}).shift(-2).equalsTo(new int[]{3, 2, 1});
+        RingInt.from(new int[]{1, 2, 3, 4, 5}).shift(-2).equalsTo(new int[]{3, 4, 5, 1, 2});
+        RingInt.from(new int[]{1, 2, 3}).shift(-4).equalsTo(new int[]{1, 2, 3});
+    }
+
+    @Test
+    public void testMatrixRotation() throws Exception {
+        Matrix m = Matrix.init(3);
+        logger.info(m.toString());
+        Matrix m1 = m.rotate90();
+        logger.info(m1.toString());
+        logger.info(String.format("%-10s","-"));
+        m = Matrix.init(4);
+        logger.info(m.toString());
+        m1 = m.rotate90();
+        logger.info(m1.toString());
+    }
+}
