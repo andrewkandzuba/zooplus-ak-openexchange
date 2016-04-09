ef4e05d05334a242f7344f891153bb23451496d8
andrew_kandzuba <andrey.kandzuba@gmail.com>
2016-04-09 18:15:44 +0300

    csrf + redis session: logout issue

---
+++ commons-rest/src/main/java/com/zooplus/openexchange/security/filters/CsrfTokenReflectionFilter.java
@@ -0,0 +1,24 @@
+package com.zooplus.openexchange.security.filters;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CsrfTokenReflectionFilter extends OncePerRequestFilter {
+    public static final String CSRF_TOKEN_HEADER = "X-CSRF-TOKEN";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        response.setHeader("X-CSRF-HEADER", token.getHeaderName());
+        response.setHeader("X-CSRF-PARAM", token.getParameterName());
+        response.setHeader("X-CSRF-TOKEN", token.getToken());
+        filterChain.doFilter(request, response);
+    }
+
+}
