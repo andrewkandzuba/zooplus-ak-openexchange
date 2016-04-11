020b18819592d51c41938006df07da4928fff209
andriyka <andriyka@playtech.com>
2016-04-11 13:50:58 +0300

    optimized paths mapping

---
+++ logic/src/test/java/com/zooplus/openexchange/starters/ApiStarter.java
@@ -0,0 +1,7 @@
+package com.zooplus.openexchange.starters;
+
+/**
+ * Created by andriyka on 4/11/2016.
+ */
+public class ApiStarter {
+}
---
+++ logic/src/test/java/com/zooplus/openexchange/tests/unit/TestWebsocketHandler.java
@@ -0,0 +1,7 @@
+package com.zooplus.openexchange.tests.unit;
+
+/**
+ * Created by andriyka on 4/11/2016.
+ */
+public class TestWebsocketHandler {
+}
