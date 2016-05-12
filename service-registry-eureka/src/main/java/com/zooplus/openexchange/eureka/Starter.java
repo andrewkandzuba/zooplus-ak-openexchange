ea198bbc8606446d1f3865fbf17ee0c5c0b3d801
andriyka <andriyka@playtech.com>
2016-05-12 16:11:19 +0300

    start exercising eureka service discovery with spring

---
+++ service-registry-eureka/src/main/java/com/zooplus/openexchange/eureka/Starter.java
@@ -0,0 +1,17 @@
+package com.zooplus.openexchange.eureka;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Starter {
+    public static void main(String... args) throws Exception {
+        new SpringApplicationBuilder()
+                .bannerMode(Banner.Mode.OFF)
+                .sources(Starter.class)
+                .run(args);
+    }
+}
