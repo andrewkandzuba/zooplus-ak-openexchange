ca01e314a1909c6730352d308a464d4a73c6bac2
andrew_kandzuba <andrey.kandzuba@gmail.com>
2016-05-28 16:11:55 +0300

    integration tests refactoring. start workout eureka client registration

--- frontend/src/main/java/com/zooplus/openexchange/Starter.java
+++ frontend/src/main/java/com/zooplus/openexchange/Starter.java
@@ -3,8 +3,10 @@ package com.zooplus.openexchange;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Starter {
     public static void main(String... args) throws Exception {
         new SpringApplicationBuilder()
